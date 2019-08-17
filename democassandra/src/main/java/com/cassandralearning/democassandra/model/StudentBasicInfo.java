036e206de57f278a5e5cdf333853a59bc3132582
laurel quadras <laurelquadras@gmail.com>
2019-08-17 19:38:34 +0530

    Fixed the application by adding getters and setters

--- democassandra/src/main/java/com/cassandralearning/democassandra/model/StudentBasicInfo.java
+++ democassandra/src/main/java/com/cassandralearning/democassandra/model/StudentBasicInfo.java
@@ -4,12 +4,12 @@ import lombok.*;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
 @ToString
 @Builder
 @Table(value = "student_basic_info")
+@Getter
+@Setter
 public class StudentBasicInfo {
 
     @PrimaryKey
@@ -22,4 +22,54 @@ public class StudentBasicInfo {
     private String location;
 
     private int age;
+
+    public StudentBasicInfo() {}
+
+    public StudentBasicInfo(int id, String name, String password, String location, int age) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.location = location;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
